baa6d6234b0539bfd3496599a2d698888868c61a
ken zhao <ken.zhao42@gmail.com>
2020-06-30 23:48:03 -0400

    Updates

---
+++ precache-manifest.53102cba793884e2e34cb7ed5c50c7a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b4a68bac20050a20437f6c8dda452f",
+    "url": "/capital-B-input/index.html"
+  },
+  {
+    "revision": "cbfec515661b8100dba0",
+    "url": "/capital-B-input/static/css/main.2cde669e.chunk.css"
+  },
+  {
+    "revision": "ddf37571aa0c89993870",
+    "url": "/capital-B-input/static/js/2.2ee385ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/capital-B-input/static/js/2.2ee385ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfec515661b8100dba0",
+    "url": "/capital-B-input/static/js/main.24c07257.chunk.js"
+  },
+  {
+    "revision": "4c3f95b3d495b1575982",
+    "url": "/capital-B-input/static/js/runtime-main.09f136be.js"
+  },
+  {
+    "revision": "0df36f74293f839903a9b524528e0449",
+    "url": "/capital-B-input/static/media/android-chrome-512x512.0df36f74.png"
+  }
+]);
